128b2e257d3675378af9c4abb7926a5b4df2eedf
Toshinori Sumida <t-sumida@web-spec.co.jp>
2021-08-23 01:51:30 +0900

    model

--- todo/models.py
+++ todo/models.py
@@ -3,9 +3,17 @@ from django.db import models
 # Create your models here.
 
 
+CHOICE = (
+    # ('DB name', 'display name')
+    ('danger', 'high'), ('warning',  'normal'), ('primary', 'low')
+)
+
+
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(max_length=50, choices=CHOICE)
+    duedate = models.DateField()
 
     # 表示名を返す
     def __str__(self):
