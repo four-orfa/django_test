d74d70ccf44124991b79973dd65ecaf1ef0bb789
Toshinori Sumida <t-sumida@web-spec.co.jp>
2021-08-26 03:08:36 +0900

    create view maked.

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import redirect, render
+from django.urls.base import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from .models import TodoModel
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -13,3 +15,11 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     template_name = "detail.html"
     model = TodoModel
+
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel
+    fields = ('title', 'memo', 'priority', 'duedate')
+    # Specify reverse'_lazy' because run after data is saved.
+    success_url = reverse_lazy('list')
